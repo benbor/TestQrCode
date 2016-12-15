e42a85960279b8ee474820fe37476b7e050da441
Barys Biankouski <biankouski@gmail.com>
2016-12-15 03:00:30 +0300

    Fix no new line at end of file.

--- src/Generator.php
+++ src/Generator.php
@@ -15,6 +15,12 @@ class Generator
         $this->renderer = $renderer;
     }
 
+    /**
+     * @param $text string Text wich should coded
+     * @param $width int
+     * @param $height int
+     * @return
+     */
     public function generate($text, $width, $height)  // text, width, height
     {
         return $this->renderer->render($text, $width, $height);
--- src/RendererInterface.php
+++ src/RendererInterface.php
@@ -4,5 +4,11 @@ namespace Ciklum\QrGenerator;
 
 interface RendererInterface
 {
+    /**
+     * @param $text string That value is a text wich should coded
+     * @param $width
+     * @param $height
+     * @return mixed
+     */
     function render($text, $width, $height);
 }
